a626635455092cd9fe50be97f284a5e103bb2ec0
marcelojsil <dev.marcelojs@gmail.com>
2024-06-19 19:32:26 -0300

    begin1

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela de Login</title>
+</head>
+<body>
+    
+    <input type="text" name="user" id="user">
+    <input type="text" name="pwd" id="pwd">
+    <input type="button" value="Entrar" name="validar" id="validar">
+
+</body>
+</html>
